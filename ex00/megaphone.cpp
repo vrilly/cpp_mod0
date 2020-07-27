8de460d6041cfd82aeefca82984ce35b1e5476ee
Tom Jans <tjans@student.codam.nl>
2020-07-27 22:33:38 +0200

    Initial commit

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,44 @@
+// Created by Tom Jans on 27/7/20.
+
+#include <iostream>
+#include <string>
+
+class Megaphone
+{
+    std::string output;
+    void capitalize_string(std::string str);
+public:
+    Megaphone(int argc, char **argv);
+    std::string get_output();
+};
+
+void Megaphone::capitalize_string(std::string str)
+{
+    for (unsigned long i = 0; i < str.length(); i++) {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - ('a' - 'A');
+    }
+    this->output += str;
+}
+
+Megaphone::Megaphone(int argc, char **argv)
+{
+    if (argc <= 1) {
+        this->output = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        return;
+    }
+    for (int i = 1; i < argc; i++)
+        capitalize_string(argv[i]);
+}
+
+std::string Megaphone::get_output()
+{
+    return this->output;
+}
+
+int main(int argc, char **argv)
+{
+    Megaphone megaphone(argc, argv);
+    std::cout << megaphone.get_output() << std::endl;
+    return 0;
+}
